1932cf8966e523d6b90960ccb08f72a161920ea4
neyadrian <neyadrian2018@gmail.com>
2026-02-05 20:25:32 -0300

    tabuada

---
+++ src/src/tabuada.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class tabuada {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+        int tabuada = 0;
+
+        System.out.println();
+        System.out.println("Tabudada do número: " + num);
+        for (int i = 0; i <= 10; i++) {
+            tabuada = i * num;
+            System.out.printf("%d x %d = %d\n", i, num, tabuada);
+        }
+
+        sc.close();
+    }
+}
