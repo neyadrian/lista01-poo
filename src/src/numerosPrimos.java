2ff343f799223aac761599d128b65a68c42ea1a6
neyadrian <neyadrian2018@gmail.com>
2026-02-05 22:00:23 -0300

    numeros primos

---
+++ src/src/numerosPrimos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// quest 10
+public class numerosPrimos {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Números primos de 1 a 100: ");
+
+        for (int i = 2; i <= 100; i++) {
+            boolean primo = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (primo) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
