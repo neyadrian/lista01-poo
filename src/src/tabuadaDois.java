99249f1dfdd4ccbe2acded79cd0146ff734137a5
neyadrian <neyadrian2018@gmail.com>
2026-02-05 22:12:28 -0300

    tabuada com do while

---
+++ src/src/tabuadaDois.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// quest 14
+public class tabuadaDois {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        int tabuada;
+
+        do {
+            System.out.print("Digite um número: ");
+            num = sc.nextInt();
+
+            if (num < 1 || num > 10) {
+                System.out.println("Erro: número fora do intervalo permitido.");
+            }
+
+        } while (num < 1 || num > 10);
+
+        System.out.println();
+        System.out.println("Tabudada do número: " + num);
+        for (int i = 0; i <= 10; i++) {
+            tabuada = i * num;
+            System.out.printf("%d x %d = %d\n", i, num, tabuada);
+        }
+
+        sc.close();
+    }
+}
