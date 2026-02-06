20eccd28d8ac3b32eeab1fdf40109ea1710aa4c6
neyadrian <neyadrian2018@gmail.com>
2026-02-05 23:06:35 -0300

    update

--- src/src/BuscarNoVetor.java
+++ src/src/BuscarNoVetor.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class BuscarNoVetor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int vetor[] = {7, 14, 3, 29, 11, 22, 5, 18, 1, 26};
+        int encontrado = 0;
+        int posicao = 0;
+
+        System.out.print("Digite um número: ");
+        int num = sc.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            if(num == vetor[i]) {
+                encontrado = 1;
+                posicao = i;
+                break;
+            }
+        }
+
+
+        if (encontrado == 1) {
+            System.out.printf("O número %d está presente no vetor na posição %d!", num, posicao);
+        } else {
+            System.out.printf("O número %d não está presente no vetor!", num);
+        }
+
+        sc.close();
+    }
 }
