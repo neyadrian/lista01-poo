4c4f576d3fff7d9024d3cb6a46c9fbb5d3384112
neyadrian <neyadrian2018@gmail.com>
2026-02-05 22:58:53 -0300

    update

--- src/src/ParesEImpares.java
+++ src/src/ParesEImpares.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 //quest 18
-public class AcharNoVetor {
+public class ParesEImpares {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
